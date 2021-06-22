a8f3a60b9264a0f52254e9958121715d88197d0d
Steve <85745298+anokapee@users.noreply.github.com>
2021-06-21 20:46:30 -0400

    whole project

---
+++ app/src/main/java/com/dynamicdevz/sendingtextbetweenactivities/MainActivity.java
@@ -0,0 +1,73 @@
+package com.dynamicdevz.sendingtextbetweenactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.dynamicdevz.sendingtextbetweenactivities.databinding.ActivityMainBinding;
+import static com.dynamicdevz.sendingtextbetweenactivities.util.Constants.DATA_KEY;
+
+public class MainActivity extends AppCompatActivity {
+    /*
+Make an application which hosts a communication between two activities.
+Both activities have an EditText and buttons used to pass the messages and
+a textview to display the conversation.
+The text should be saved to sharedpreferences and should be made available
+when the application is restarted.
+- Use styles to make your application presentable
+- do not use raw values, all values(Strings, dimens) should be extracted
+- use Glide to display a "user icon" in both activities
+*/
+    private ActivityMainBinding binding;
+
+    private SharedPreferences sharedPreferences;
+    private String data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Glide.with(MainActivity.this)
+                .load(R.drawable.profile_one_pic)
+                .into(binding.profileIv);
+        sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        readFromSharedPref();
+
+        binding.sendBtn.setOnClickListener(view -> {
+            String message = binding.messageEt.getText().toString().trim();
+            //Clear text
+            binding.convoTv.setText("");
+
+
+            if(message.length() == 0) {
+                Toast.makeText(this, "Cannot send and empty message", Toast.LENGTH_LONG).show();
+                readFromSharedPref();
+            }
+            else {
+                if(data.equals("No messages"))
+                    data = "";
+
+                sharedPreferences.edit()
+                        .putString(DATA_KEY, data+"\n"+message+"\n")
+                        .apply();
+                readFromSharedPref();
+
+                Intent subIntent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(subIntent);
+
+            }
+
+        });
+    }
+
+    private void readFromSharedPref() {
+        data = sharedPreferences.getString(DATA_KEY, "No messages");
+        binding.convoTv.setText(data);
+    }
+}
---
+++ app/src/main/java/com/dynamicdevz/sendingtextbetweenactivities/util/Constants.java
@@ -0,0 +1,5 @@
+package com.dynamicdevz.sendingtextbetweenactivities.util;
+
+public class Constants {
+    public static final String DATA_KEY = "KEY";
+}
